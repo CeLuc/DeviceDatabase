ce47fb0aca2f465fe8a1e04a8b8a4c7d5adcf726
thomas-blakeney <blaumc@icloud.com>
2022-05-27 13:28:45 +0100

    feat: backend added pc example feat: added db page

--- Backend/index.js
+++ Backend/index.js
@@ -6,36 +6,38 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type PC {
+    _id: ID
+    hostname: String
+    staticip: Boolean
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    pcs: [PC]
   }
 `;
 
-const books = [
+const pcs = [
   {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
+    _id: "fdjkfhasif",
+    hostname: "A-BJE-P0094",
+    staticip: true,
   },
   {
-    title: 'City of Glass',
-    author: 'Paul Auster',
+    _id: "ncfkjncfknfgsdnnkf",
+    hostname: "A-BJE-0105",
+    staticip: true,
   },
-];
+]
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    pcs: () => pcs,
   },
 };
 
