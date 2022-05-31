6a67a3dc1d3a85c2fc69e99fbc09b92ddb870e39
GruberL <ceo@firecave.de>
2022-05-31 16:26:43 +0200

    feat(frontend): added more data to query

--- Frontend/api/queries.js
+++ Frontend/api/queries.js
@@ -3,7 +3,17 @@ import { gql } from "@apollo/client/core";
 export const GET_POSTS = gql`
   query get_posts {
     pcs {
+      id
       hostname
+      room {
+        name
+      }
+      network {
+        name
+      }
+      house {
+        number
+      }
     }
   }
 `;
