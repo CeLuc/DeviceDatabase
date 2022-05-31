7534246157197f62b12189377fa3841f7a7d87e6
GruberL <ceo@firecave.de>
2022-05-31 16:26:57 +0200

    feat(frontend):tailwind customized theme

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -8,26 +8,17 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    // darkSelector: "dark-mode",
-    extend: {},
+    // colors: {
+    //   white: "#fff",
+    //   gray: "#696969",
+    //   "gray-light": "#747678",
+    //   black: "#000",
+    // },
+    extend: {
+      colors: {
+        primary: "#E10E4B",
+        secondary: "#009581",
+      },
+    },
   },
-  // variants: {
-  //   backgroundColor: [
-  //     "dark",
-  //     "dark-hover",
-  //     "dark-group-hover",
-  //     "dark-even",
-  //     "dark-odd",
-  //     "hover",
-  //     "responsive",
-  //   ],
-  //   borderColor: [
-  //     "dark",
-  //     "dark-focus",
-  //     "dark-focus-within",
-  //     "hover",
-  //     "responsive",
-  //   ],
-  //   textColor: ["dark", "dark-hover", "dark-active", "hover", "responsive"],
-  // },
 };
