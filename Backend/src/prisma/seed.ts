c46350e247471d4040376a6224dccf6e89c88ed4
GruberL <ceo@firecave.de>
2022-06-02 17:28:55 +0200

    feat(Backend): added seeding file for prisma

---
+++ Backend/src/prisma/seed.ts
@@ -0,0 +1,101 @@
+import { PrismaClient } from "@prisma/client";
+const { prisma } = require("./client");
+
+async function main() {
+  const h1 = await prisma.House.upsert({
+    where: { number: 1 },
+    update: {},
+    create: {
+      number: 1,
+    },
+  });
+  const r1 = await prisma.Room.upsert({
+    where: { name: "RoomtEst1" },
+    update: {},
+    create: {
+      name: "RoomtEst1",
+      house: {
+        connect: {
+          number: 1,
+        },
+      },
+    },
+    include: {
+      house: true,
+    },
+  });
+  const fse = await prisma.Network.upsert({
+    where: { name: "FSE" },
+    update: {},
+    create: {
+      name: "FSE",
+    },
+  });
+  const p0105 = await prisma.PC.upsert({
+    where: { hostname: "A-BJE-P0105" },
+    update: {},
+    create: {
+      hostname: "A-BJE-P0105",
+      staticip: false,
+      network: {
+        connect: {
+          name: "FSE",
+        },
+      },
+      house: {
+        connect: {
+          number: 1,
+        },
+      },
+      room: {
+        connect: {
+          name: "RoomtEst1",
+        },
+      },
+    },
+    include: {
+      network: true,
+      house: true,
+      room: true,
+    },
+  });
+
+  const p0102 = await prisma.PC.upsert({
+    where: { hostname: "A-BJE-P0102" },
+    update: {},
+    create: {
+      hostname: "A-BJE-P0102",
+      staticip: false,
+      network: {
+        connect: {
+          name: "FSE",
+        },
+      },
+      house: {
+        connect: {
+          number: 1,
+        },
+      },
+      room: {
+        connect: {
+          name: "RoomtEst1",
+        },
+      },
+    },
+    include: {
+      network: true,
+      house: true,
+      room: true,
+    },
+  });
+  console.log({ h1, r1, fse, p0105, p0102 });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
