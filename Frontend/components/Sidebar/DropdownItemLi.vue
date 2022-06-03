763be5ce5a266a82e5b52e5b4f3527a21c64d898
GruberL <ceo@firecave.de>
2022-06-03 22:12:43 +0200

    feat(Frontend): added dropdown-menu component for sidebar

---
+++ Frontend/components/Sidebar/DropdownItemLi.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps(["name", "color", "link"]);
+</script>
+
+<template>
+  <li class="my-px text-gray-300 first:mt-0 group">
+    <NuxtLink
+      :to="{ path: link }"
+      activeClass="text-gray-700 bg-gray-100"
+      class="flex flex-row items-center h-10 px-3 transition-all duration-200 hover:ease-in hover:bg-gray-100 hover:text-gray-700 group-last:rounded-b-xl"
+    >
+      <span
+        class="flex items-center justify-center text-lg"
+        :class="[color != null ? color : 'text-gray-400']"
+      >
+        <div class="w-6 h-6 fill-current">
+          <slot />
+        </div>
+      </span>
+      <span class="ml-3">{{ name }}</span>
+    </NuxtLink>
+  </li>
+</template>
