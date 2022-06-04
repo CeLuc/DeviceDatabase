47aefab1809e458be6663fe365c9fc497268719c
GruberL <ceo@firecave.de>
2022-06-04 18:56:17 +0200

    style(Frontend): removed comment from breadcrumb component

--- Frontend/components/Breadcrumbs.vue
+++ Frontend/components/Breadcrumbs.vue
@@ -3,7 +3,9 @@ const router = useRouter();
 </script>
 <template>
   <div class="flex items-center ml-4">
-    <div class="flex items-center pb-6 overflow-y-auto whitespace-nowrap">
+    <div
+      class="flex items-center pb-6 overflow-y-auto uppercase whitespace-nowrap"
+    >
       <NuxtLink
         to="/"
         class="flex items-center text-gray-600 hover:underline dark:text-gray-200"
@@ -46,50 +48,10 @@ const router = useRouter();
             :to="item.path"
             class="text-gray-600 dark:text-gray-200 hover:underline group-last:text-blue-600 dark:group-last:text-blue-400"
           >
-            {{
-              item.path.substring(item.path.lastIndexOf("/") + 1).toUpperCase()
-            }}
+            {{ item.path.substring(item.path.lastIndexOf("/") + 1) }}
           </NuxtLink>
         </div>
       </div>
-
-      <!-- <span class="mx-5 text-gray-500 dark:text-gray-300">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-5 h-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </span>
-
-      <a href="#" class="text-gray-600 dark:text-gray-200 hover:underline">
-        Profile
-      </a>
-
-      <span class="mx-5 text-gray-500 dark:text-gray-300">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-5 h-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </span>
-
-      <a href="#" class="text-blue-600 dark:text-blue-400 hover:underline">
-        Settings
-      </a> -->
     </div>
   </div>
 </template>
