468f8eb6ae9f3ccfb6eea5b0028a61ec87809746
GruberL <ceo@firecave.de>
2022-06-04 18:55:05 +0200

    style(Frontend): removed wrong svg props

--- Frontend/components/Sidebar.vue
+++ Frontend/components/Sidebar.vue
@@ -11,9 +11,7 @@
             viewBox="0 0 20 20"
           >
             <path
-              fill-rule="evenodd"
               d="M11.757 2.034a1 1 0 01.638.519c.483.967.844 1.554 1.207 2.03.368.482.756.876 1.348 1.467A6.985 6.985 0 0117 11a7.002 7.002 0 01-14 0c0-1.79.684-3.583 2.05-4.95a1 1 0 011.707.707c0 1.12.07 1.973.398 2.654.18.374.461.74.945 1.067.116-1.061.328-2.354.614-3.58.225-.966.505-1.93.839-2.734.167-.403.356-.785.57-1.116.208-.322.476-.649.822-.88a1 1 0 01.812-.134zm.364 13.087A2.998 2.998 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879.586.585.879 1.353.879 2.121s-.293 1.536-.879 2.121z"
-              clip-rule="evenodd"
             />
           </svg>
           <span
@@ -28,11 +26,9 @@
         <SidebarSingleLi name="Dashboard" link="/">
           <svg
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
             viewBox="0 0 24 24"
             stroke="currentColor"
+            class="stroke-2"
           >
             <path
               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
@@ -96,11 +92,9 @@
         <SidebarSingleLi name="Manager" link="/manager">
           <svg
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
             viewBox="0 0 24 24"
             stroke="currentColor"
+            class="stroke-2"
           >
             <path
               d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
@@ -130,12 +124,9 @@
         <SidebarSingleLi name="Profile" link="/profile">
           <svg
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="w-6 h-6 stroke-2"
           >
             <path
               d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
@@ -145,12 +136,9 @@
         <SidebarSingleLi name="Settings" link="/settings">
           <svg
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="w-6 h-6 stroke-2"
           >
             <path
               d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
@@ -161,12 +149,9 @@
         <SidebarSingleLi name="Logout" color="text-red-400" link="/logout">
           <svg
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="w-6 h-6 stroke-2"
           >
             <path
               d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"
