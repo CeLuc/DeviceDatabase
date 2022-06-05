c6c0028140fee2455c4a4a7a0b7acd4b75ea392b
GruberL <ceo@firecave.de>
2022-06-05 17:21:32 +0200

    feat(Backend): added additional houses, rooms, networks and pc for seeding

--- Backend/src/prisma/seed.ts
+++ Backend/src/prisma/seed.ts
@@ -9,6 +9,13 @@ async function main() {
       number: 1,
     },
   });
+  const h8 = await prisma.House.upsert({
+    where: { number: 8 },
+    update: {},
+    create: {
+      number: 8,
+    },
+  });
 
   const r1 = await prisma.Room.upsert({
     where: { name: "testRoom1" },
@@ -25,12 +32,34 @@ async function main() {
       house: true,
     },
   });
+  const sr3 = await prisma.Room.upsert({
+    where: { name: "Schulungsraum 3" },
+    update: {},
+    create: {
+      name: "Schulungsraum 3",
+      house: {
+        connect: {
+          number: 8,
+        },
+      },
+    },
+    include: {
+      house: true,
+    },
+  });
 
   const fse = await prisma.Network.upsert({
     where: { name: "FSE" },
     update: {},
     create: {
       name: "FSE",
+    },
+  });
+  const s03 = await prisma.Network.upsert({
+    where: { name: "Schul03" },
+    update: {},
+    create: {
+      name: "Schul03",
     },
   });
 
@@ -90,7 +119,63 @@ async function main() {
       room: true,
     },
   });
-  console.log({ h1, r1, fse, p0094, p0105 });
+  const ps03r301 = await prisma.PC.upsert({
+    where: { hostname: "A-BJE-PS03R3-1" },
+    update: {},
+    create: {
+      hostname: "A-BJE-PS03R3-1",
+      staticip: false,
+      network: {
+        connect: {
+          name: "FSE",
+        },
+      },
+      house: {
+        connect: {
+          number: 8,
+        },
+      },
+      room: {
+        connect: {
+          name: "Schulungsraum 3",
+        },
+      },
+    },
+    include: {
+      network: true,
+      house: true,
+      room: true,
+    },
+  });
+  const ps03r302 = await prisma.PC.upsert({
+    where: { hostname: "A-BJE-PS03R3-2" },
+    update: {},
+    create: {
+      hostname: "A-BJE-PS03R3-2",
+      staticip: false,
+      network: {
+        connect: {
+          name: "Schul03",
+        },
+      },
+      house: {
+        connect: {
+          number: 8,
+        },
+      },
+      room: {
+        connect: {
+          name: "Schulungsraum 3",
+        },
+      },
+    },
+    include: {
+      network: true,
+      house: true,
+      room: true,
+    },
+  });
+  console.log({ h1, h8, r1, sr3, fse, s03, p0094, p0105, ps03r301, ps03r302 });
 }
 
 main()
