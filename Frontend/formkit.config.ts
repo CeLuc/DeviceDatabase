cd6b2ca884f9352568aaab3af9aa029e2f0d94a0
GruberL <admin@firecave.de>
2022-07-04 20:18:24 +0200

    feat(Frontend): Added Formkit and Tailwind Forms

---
+++ Frontend/formkit.config.ts
@@ -0,0 +1,9 @@
+import { de, en } from "@formkit/i18n";
+import { DefaultConfigOptions } from "@formkit/vue";
+
+const config: DefaultConfigOptions = {
+  locales: { de, en },
+  locale: "de",
+};
+
+export default config;
