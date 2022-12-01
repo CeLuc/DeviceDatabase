b1d0f59a5b2f1c56b6d320cdc972425e0677e142
Thomas Blakeney <blaumc@icloud.com>
2022-12-01 14:46:37 +0000

    tailwind and prisma initialized

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
 
+  runtimeConfig: {
+    apiSecret: process.env.API_SECRET,
+  }
 })
