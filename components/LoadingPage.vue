c2152ced1dd34850707378968ecbc1c19b94bbb2
Lucas Gruber <blaumc@icloud.com>
2022-12-07 13:35:24 +0100

    fixed loading

--- components/LoadingPage.vue
+++ components/LoadingPage.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex items-center justify-center h-screen">
-    <div class="absolute w-20 h-20 animate-ping">
+    <!-- <div class="absolute w-20 h-20 animate-ping">
       <LogoTwitter />
-    </div>
+    </div> -->
+    Loading...
   </div>
 </template>
